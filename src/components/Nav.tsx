bce65453219a8fd25a26e071d6b0e5ac9b732f26
Ivancho205 <ivan.velasquez092@pascualbravo.edu.co>
2026-06-23 17:42:35 -0500

    Actualización que no se habia podido subir el lunes

---
+++ src/components/Nav.tsx
@@ -0,0 +1,18 @@
+function Nav (){
+    return(
+        <nav>
+      <h2>TicketHub</h2>
+
+      <ul>
+        <li>Explorar Eventos</li>
+        <li>Vender Boleta</li>
+        <li>Mis Publicaciones</li>
+        <li>Soporte</li>
+        <li>Iniciar Sesión</li>
+      </ul>
+    </nav>
+
+    );
+
+}
+export default Nav;
