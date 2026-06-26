1bc0460b93a0cb2f17d072d02e01fae9c11378c8
Ivancho205 <ivan.velasquez092@pascualbravo.edu.co>
2026-06-26 12:31:37 -0500

    Creado Hero inicial de la página Home

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css'
 import { ApertureTransition } from './components/ApertureTransition'
 import { Footer } from './components/Footer'
 import Nav from './components/Nav'
+import Home from './pages/Home'
+
 
 function App() {
   return (
     <>
       <ApertureTransition key={location.pathname} duration={900} />
       <Nav></Nav>
+      <Home />
       <Footer companyName="ticketus"></Footer>
     </>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,26 @@
-import viteLogo  from '../../public/favicon.svg'
-import reactLogo from '../assets/react.svg'
+import "./Home.css";
 
-export default function Home() {
-  return (
-    <div id="app_container" className="app-container">
-        <section id="up_center" className="section_all">
-            <div className="">
-                <img src={reactLogo} className="" alt="React logo" />
-                <img src={viteLogo} className="" alt="Vite logo" />
-            </div>
-            <div>
-                <h1>Get started</h1>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-                </p>
-            </div>
-        </section>
-    </div>
-  );
+function Home(){
+    return(
+        <main className="home">
+
+      <section className="hero">
+
+        <h1>TicketHub</h1>
+
+        <h2>Compra y vende boletas de forma segura</h2>
+
+        <p>
+          Encuentra entradas para conciertos, festivales,
+          eventos deportivos y mucho más.
+        </p>
+
+        <button>Explorar Eventos</button>
+
+      </section>
+
+    </main>
+    
+    );
 }
+export default Home;
