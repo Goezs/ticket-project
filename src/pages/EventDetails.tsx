e0b2252d9e4b703cef3ca264cb6a9e490fd0544f
Ivancho205 <ivan.velasquez092@pascualbravo.edu.co>
2026-07-21 17:05:21 -0500

    inicio de la pantalla para hacer la compra de los tickets

---
+++ src/pages/EventDetails.tsx
@@ -0,0 +1,15 @@
+import "./EventDetails.css";
+
+function EventDetails(){
+    return (
+        <main className="event-details">
+
+        <section className="event-card">
+
+        </section>
+        
+        </main>
+        
+    );
+}
+export default EventDetails;
