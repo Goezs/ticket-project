a18603499b23fd844ee0fd8573def91680a8ecad
Goez <andres.mauricio.goez.yepes@gmail.com>
2026-07-30 20:17:58 -0500

    Description of event
    EvenDetails
        - Stylish information
        - All information of event including datetimes

--- src/pages/EventDetails.tsx
+++ src/pages/EventDetails.tsx
@@ -46,29 +46,59 @@ function EventDetails() {
   };
 
   return (
-    <main className="flex flex-col md:flex-row max-w-2x1 bg-white dark:bg-slate-">
-      {/* Image container */}
-      <div className="relative w-full md:w-full h-full object-cover">
-        <img
-          className="absolute inset-0 w-full md:w-2/5 h-48 md:h-auto min-h-[180px]"
-          src={getImageUrl(event.imageUrl)}
-          alt={event.name}
-          loading="lazy"
-        />
-      </div>
+    <main className="rounded-2xl border border-gray-200 bg-white px-5 pb-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6" >
+      <div className="flex max-w-2x1 ">
+        {/* Image container */}
+        <div className="w-full md:w-full h-full object-cover justify-center">
+          <img
+            className="min-h-[180px] rounded-2xl"
+            src={getImageUrl(event.imageUrl)}
+            alt={event.name}
+            loading="lazy"
+          />
+        </div>
 
-      {/* Content container */}
-      <div className="flex flex-col justify-between p-6 md:w-3/5">
-        <div className="space-y-2">
-          {/* Name/Title */}
-          <h3 className="text-xl font-bold text-slate-900 dark:text-white tracking-tight">
-            {event.name}
-          </h3>
+        {/* Content container */}
+        <div className="flex justify-left p-4 ">
+          <div className="space-y-2">
+            {/* Name/Title */}
+            <h3 className="text-4xl font-bold text-slate-900 dark:text-white tracking-tight">
+              {event.name}
+            </h3>
 
-          {/* Description */}
-          <p className="text-sm text-slate-600 dark:text-slate-400 leading-relax line-clamp-4">
-            {event.description}
-          </p>
+            {/* Description */}
+            <p className="text-sm text-slate-600 dark:text-slate-400 leading-relax line-clamp-4">
+              {event.description}
+            </p>
+
+            {/* Genre */}
+            <p className="text-sm text-slate-600 dark:text-slate-400 leading-relax line-clamp-4 pt-6">
+              {event.category}
+            </p>
+
+            {/* Place */}
+            <p className="text-sm text-slate-600 dark:text-slate-400 leading-relax line-clamp-4 pt-3">
+              {event.place}
+            </p>
+
+            {/* Author */}
+            <p className="text-sm text-slate-600 dark:text-slate-400 leading-relax line-clamp-4 pt-3">
+              {event.author}
+            </p>
+
+            {/* Date */}
+            <p className="text-sm text-slate-600 dark:text-slate-400 leading-relax line-clamp-4 pt-3">
+              Selling date: {event.sellingDate?.toString()}
+            </p>
+
+            {/* Start hour */}
+            <p className="text-sm text-slate-600 dark:text-slate-400 leading-relax line-clamp-4 pt-3">
+              Start hour: {event.startHour?.toTimeString()}
+            </p>
+
+
+
+          </div>
         </div>
       </div>
     </main>
